5aeef3832fee6fc980e62dc764170777de14c28c
Teiji <tjs392@drexel.edu>
2023-07-03 11:28:11 -0400

    Changes

---
+++ src/movegen.h
@@ -0,0 +1,19 @@
+#ifndef MOVEGEN_H
+#define MOVEGEN_H
+
+#include "bitboards.h"
+#include <vector>
+#include <cstdint>
+
+
+struct Move {
+    int origin;
+    int target;
+
+};
+//Generate moves for each piece
+//General move gen functions
+std::vector<
+
+
+#endif
