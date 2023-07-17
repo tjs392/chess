f6169c3198d111e1df0c1c3ba93bade5418d6298
tjs392 <tjs392@drexel.edu>
2023-07-16 20:59:42 -0400

    Finished pseudo-legal for pawn gen, except for en passant

--- src/lazygen.h
+++ src/lazygen.h
@@ -25,6 +25,7 @@ struct PawnMove{
 //Generation for pawn moves will be on-demand
 //IE moves will not be all generated at once at the start of the turn
 
+//Targets and origins will also be stored as ints for simplicity sake
 std::vector<PawnMove> genPawnMove(Board& board, int origin, bool whiteToMove){
 
     std::vector<PawnMove> moves;
@@ -34,8 +35,9 @@ std::vector<PawnMove> genPawnMove(Board& board, int origin, bool whiteToMove){
 
     Bitboard origin_BB = 1ULL << origin;
 
-    Bitboard single_forward = (whiteToMove) ? (origin_BB << 8) & board.occupied : origin_BB >> 8 & board.occupied;
-    if (single_forward != 0){
+    //Single and double forward moves
+    Bitboard single_forward = (whiteToMove) ? (origin_BB << 8) & ~board.occupied : origin_BB >> 8 & ~board.occupied;
+    if (single_forward){
         PawnMove single;
         single.target = (whiteToMove) ? (origin + 8) : (origin - 8);
         single.origin = origin;
@@ -44,8 +46,8 @@ std::vector<PawnMove> genPawnMove(Board& board, int origin, bool whiteToMove){
 
         bool canDouble = (whiteToMove) ? (origin_BB & Bitboards::Rank2) : (origin_BB & Bitboards::Rank7);
         if (canDouble){
-            Bitboard double_forward = (whiteToMove) ? (single_forward << 8) & board.occupied : (single_forward >> 8) & board.occupied;
-            if (double_forward != 0){
+            Bitboard double_forward = (whiteToMove) ? (single_forward << 8) & ~board.occupied : (single_forward >> 8) & ~board.occupied;
+            if (double_forward){
                 PawnMove double_m;
                 double_m.target = (whiteToMove) ? (single.target + 8) : (single.target - 8);
                 double_m.origin = origin;
@@ -54,8 +56,29 @@ std::vector<PawnMove> genPawnMove(Board& board, int origin, bool whiteToMove){
             }
         }
     }
+    //Left and right capture
+    Bitboard left_capture = (whiteToMove) ? (origin_BB << 7) & board.occupied : (origin_BB >> 7) & board.occupied;
+    Bitboard right_capture = (whiteToMove) ? (origin_BB << 9) & board.occupied : (origin_BB >> 9) & board.occupied;
 
-};
+    if (left_capture){
+        PawnMove left_cap;
+        left_cap.target = (whiteToMove) ? (origin + 7) : (origin - 7);
+        left_cap.origin = origin;
+        left_cap.moveType = CAPTURE;
+        moves.push_back(left_cap);
+    }
+    if (right_capture){
+        PawnMove right_cap;
+        right_cap.target = (whiteToMove) ? (origin + 9) : (origin - 9);
+        right_cap.origin = origin;
+        right_cap.moveType = CAPTURE;
+        moves.push_back(right_cap);
+    }
+
+    return moves;
+
+    //Deal with promotion and en passant later, I will need something to store previous moves** :)
+}
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include "bitboards.h"
 #include "display.h"
 #include <iostream>
+#include "lazygen.h"
 
 int main(){
     Board board;
     board.init();
     displayBoard(board);
+
+    std::vector<PawnMove> moves = genPawnMove(board, 10, board.whiteToMove);
+
+    for (int i = 0; i < moves.size(); i++){
+        std::cout << moves[i].target << std::endl;
+    }
+
+    std::cout << moves.size() << std::endl;
 
     return 0;
 }
