42ff6ac52dbe2e5064a79f87fa09e0332ae613a1
tjs392 <tjs392@drexel.edu>
2023-08-13 13:16:30 -0400

    Finished move gen, added test conditions

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,47 @@
 #include <chrono>
 #include <bitset>
 #include "sliding.h"
+#include <string>
 
 int main() {
     Board board;
     board.init();
     board.clear();
+    //board.fill(Sliding::bishopPseudoGen(1), true, BISHOP);
+    //displayBoard(board);
     //board.fill(EW_rook_mask(0), true, ROOK);
     //board.fill(NS_rook_mask(53), true, ROOK);
-    displayBoard(board);
+    /*for (int i = 63; i >= 0; i--){
+        board.fill(Sliding::bishopPseudoGen(i), true, BISHOP);
+        //std::cout << (Sliding::diagonal_mask(i)) << std::endl;
+        displayBoard(board);
+        board.clear();
+    }*/
+    //std::cout << (Sliding::diagonal_mask(i)) << std::endl;
+    
+
+    int input;
+
+    while (true) {
+        std::cout << "Enter an index (0-63) or '-1' to exit: ";
+        std::cin >> input;
+        
+        if (input == -1) {
+            break;
+        }
+        
+
+        if (input >= 0 && input < 64) {
+            board.fill(Sliding::queenPseudoGen(input), true, BISHOP);
+            
+            std::cout << "Mask for Index " << input << ":" << std::endl;
+            displayBoard(board);
+        
+        } else {
+            std::cout << "Invalid input. Please enter an index between 0 and 63." << std::endl;
+        }
+        board.clear();
+    }
 
     
 
--- src/sliding.h
+++ src/sliding.h
@@ -1,20 +1,70 @@
 #include <cstdint>
 #include "bitboards.h"
-#include <vector>
-
+#include <array>
+\
 using Bitboard = uint64_t;
 
+namespace Sliding{
+    
+    //Store precomputed diagonal attacks
+    
+    std::vector<Bitboard> main_diagonal(64);
+    std::vector<Bitboard> anti_diagonal(64);
 
-//Rook masks
-Bitboard NS_rook_mask(int origin){
-    return 0x101010101010101 << ((origin % 8));
-}
 
-Bitboard EW_rook_mask(int origin){
-    return 0xffULL << (8 * (origin / 8));
-}
-//Making rook and bishop non-blocking move gen
+    //On demand diagonal
+    Bitboard diagonal_mask(int origin) {
+        Bitboard mask = 0;
 
-Bitboard rookPseudoGen(Board& board, int origin){
-    return 0;
+        int file = origin % 8;
+        int rank = origin / 8;
+        int start = origin - (std::min(rank, file) * 9);
+
+        for (int i = start; i < 64; i+=9){
+            mask |= (1ULL << (i));
+            if (i % 8 == 7 || i / 8 == 7){return mask;}
+        }
+        return -1;
+    }
+
+    Bitboard anti_diagonal_mask(int origin) {
+        Bitboard mask = 0;
+
+        int file = origin % 8;
+        int rank = origin / 8;
+        int start = origin - (std::min(rank, 7 - file) * 7);
+
+        for (int i = start; i < 64; i += 7) {
+            mask |= (1ULL << i);
+            if (i % 8 == 0 || i / 8 == 7) {
+                break;
+            }
+        }
+        return mask;
+    }
+    // On demand North-South and East_West masks
+    Bitboard NS_mask(int origin){
+        return 0x101010101010101 << ((origin % 8));
+    }
+
+    Bitboard EW_mask(int origin){
+        return 0xffULL << (8 * (origin / 8));
+    }
+
+
+
+
+    //Making sliding piece non-blocking move gen
+
+    Bitboard rookPseudoGen(int origin){
+        return NS_mask(origin) | EW_mask(origin);
+    }
+
+    Bitboard bishopPseudoGen(int origin){
+        return diagonal_mask(origin) | anti_diagonal_mask(origin);
+    }
+
+    Bitboard queenPseudoGen(int origin){
+        return NS_mask(origin) | EW_mask(origin) | diagonal_mask(origin) | anti_diagonal_mask(origin);
+    }
 }
